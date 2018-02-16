691f053de03384c5f39eacb3972e187ae40454f6
stoopfrench <32528005+stoopfrench@users.noreply.github.com>
2018-02-15 22:59:06 -0700

    minor update

--- api/models/movieModel.js
+++ api/models/movieModel.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const movieSchema = mongoose.Schema({
 	id: {type: Number, required: true, unique: true},
 	title: {type: String, required: true, unique: true},
 	year: {type: Number, required: true},
 	genres: {type: String, required: true}
-});
+})
 
-module.exports = mongoose.model('Movie', movieSchema);
+module.exports = mongoose.model('Movie', movieSchema)
