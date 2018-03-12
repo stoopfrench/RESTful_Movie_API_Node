614e5ef8f07ff5a8f78f2b0e120f721003326f62
stoopfrench <32528005+stoopfrench@users.noreply.github.com>
2018-03-11 18:00:08 -0600

    changed child-process to promises

--- db_utilities/exportToCsv.js
+++ db_utilities/exportToCsv.js
@@ -1,8 +1,8 @@
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
-var tOffset = (new Date()).getTimezoneOffset() * 60000;
-var now = (new Date(Date.now() - tOffset)).toISOString().slice(0, -5);
+var tOffset = (new Date()).getTimezoneOffset() * 60000
+var now = (new Date(Date.now() - tOffset)).toISOString().slice(0, -5)
 
 exec(`mongoexport --db movie-database --collection movies --type csv --fields index,title,year,genres --out ./db_utilities/csv_files/exported/EXPORTED_movies_${now}.csv`)
     .then((stdout) => {
@@ -15,5 +15,5 @@ exec(`mongoexport --db movie-database --collection movies --type csv --fields in
 
     })
     .catch(error => {
-        console.log(error)  
+        console.log(error)
     })
--- db_utilities/seeder.js
+++ db_utilities/seeder.js
@@ -1,35 +1,38 @@
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 const config = require('config')
-const mongoose = require('mongoose')
 
+const mongoose = require('mongoose')
 const Movie = require('../api/models/movieModel')
 
 mongoose.connect(config.dbHost)
 
 const csv_file = process.argv[2] || "Movie-List.csv"
-    if(csv_file.startsWith("EXPORTED")) {
-        exec(`
+if (csv_file.startsWith("EXPORTED")) {
+    exec(`
             cp ./db_utilities/csv_files/exported/${csv_file} ./db_utilities/csv_files/${csv_file}
             sed -i '' 1d ./db_utilities/csv_files/${csv_file}
-            `)
-    }
+        `)
+        .then(() => {
+            console.log("...csv headerline has been removed")
+        })
+        .catch(error => {
+            console.log("file not transformed", error)
+        })
+}
 
 exec(`
     echo;
     echo DUMPING OLD DATABASE;
     echo ============================================================;
     mongo movie-database --eval "db.dropDatabase()";
-    `,
-    (error, stdout, stderr) => {
-        if (stdout) {
-            console.log(stdout)
+    `)
+    .then(stdout => {
+        if (stdout.stdout) {
+            console.log(stdout.stdout)
         }
-        if (stderr) {
-            console.log(stderr)
-        }
-        if (error !== null) {
-            console.log(`exec error: ${error}`)
+        if (stdout.stderr) {
+            console.log(stdout.stderr)
         }
         console.log("...done")
 
@@ -51,6 +54,9 @@ exec(`
                     console.log(err)
                 })
         })
+    })
+    .catch(error => {
+        console.log(error)
     })
 
 
